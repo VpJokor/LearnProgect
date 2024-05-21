8180a09dd74d71df8afee3440be859f28be91009
宋志文 <78901214@qq.com>
2024-05-21 18:15:37 +0800

    UIKit纯 代码布局

--- LearnProgect/ViewController.swift
+++ LearnProgect/ViewController.swift
@@ -11,8 +11,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    
+        
+
+        view.backgroundColor = UIColor.white
+
+        
+        // 创建一个 Label
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 创建一个 Button
+        let button = UIButton(type: .system)
+        button.setTitle("Press Me", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 添加 Label 和 Button 到视图中
+        view.addSubview(label)
+        view.addSubview(button)
+        
+        // 设置 Label 的布局约束
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50)
+        ])
+        
+        // 设置 Button 的布局约束
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
         
     }
 
